d2513deed0f28a93d8ef79fdd8dcb0b6cb40d862
Vasudev suthar <doyalvasudev87@gmail.com>
2024-02-04 22:13:35 -0800

    util code discussion

---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,23 @@
+const asyncHandler = (reQuestHandler) => {
+    (req, res, next) => {
+        Promise.resolve(reQuestHandler(req, res, next)).catch((err) => next(err))
+    }
+}
+
+export {asyncHandler}
+
+
+// const asyncHandler = () => {}
+// const asyncHandler = (function) => () =>  {}
+// const asyncHandler = (function) => async () => {}
+
+// const asyncHandler = (fn) => async (req, res , next) => {
+//     try {
+//         await fn(req,res,next)
+//     } catch (error) {
+//         res.status(err.code || 500).json({
+//             success: false,
+//             message: err.message
+//         })
+//     }
+// }
